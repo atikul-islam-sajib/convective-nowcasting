9e85ea0a0a22fea05ff75ee531c308563bf71eec
Atikul Islam Sajib <atikulislamsajib137@gmail.com>
2026-02-19 16:39:37 +0100

    time utils is created and it is determined whether the past sequence of satellite image and radar image is correct or not, it also works for future satellite image and radar image as well

---
+++ src/utils/time_utils.py
@@ -0,0 +1,27 @@
+from datetime import timedelta
+
+def get_satellite_history_and_radar_target(
+    reference_time,
+    cadence_minutes,
+    history_minutes,
+    lead_minutes,
+):
+    """
+    reference_time = LAST satellite timestamp (t)
+
+    Satellite history:
+      t-25, t-20, t-15, t-10, t-5, t   (for 30 min, 5-min cadence)
+
+    Radar target:
+      t + lead_minutes
+    """
+    n_steps = history_minutes // cadence_minutes  # 6
+
+    satellite_times = [
+        reference_time - timedelta(minutes=(n_steps - 1 - i) * cadence_minutes)
+        for i in range(n_steps)
+    ]
+
+    radar_target_time = reference_time + timedelta(minutes=lead_minutes)
+
+    return satellite_times, radar_target_time
